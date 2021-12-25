1192bf30ce79a43f891f0799b4455cab03fcccf0
Uwe Fechner <fechner@aenarete.eu>
2021-12-25 15:00:54 +0100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using KiteUtils
 using Test
 
+cd("..")
+
 @testset "KiteUtils.jl" begin
     # Write your tests here.
     @test se().project == "settings.yaml"
     @test se().log_file == "data/log_8700W_8ms"
     @test length(se().alpha_cl) == 12
 end
+nothing
