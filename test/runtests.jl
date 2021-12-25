7008dccb120dd810c3f3ac074761bc6275c8bc42
Uwe Fechner <fechner@aenarete.eu>
2021-12-25 17:30:24 +0100

    Add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,8 @@ cd("..")
     @test se().project == "settings.yaml"
     @test se().log_file == "data/log_8700W_8ms"
     @test length(se().alpha_cl) == 12
+    log = KiteUtils.test()
+    @test typeof(log) == SysLog{7}
+    @test log.syslog.Z[end][7] ≈ 6 # height of the last particle which represents the kite
 end
 nothing
