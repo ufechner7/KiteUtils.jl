3157913ad2599315ed3eca623916bb1c46d19e27
Uwe Fechner <fechner@aenarete.eu>
2021-12-25 14:36:33 +0100

    Initial release

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,7 @@ using Test
 
 @testset "KiteUtils.jl" begin
     # Write your tests here.
+    @test se().project == "settings.yaml"
+    @test se().log_file == "data/log_8700W_8ms"
+    @test length(se().alpha_cl) == 12
 end
