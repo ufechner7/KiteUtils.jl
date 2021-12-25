4f2b4a663a836a3e6a459fe5ee749f1d0b272890
Documenter.jl <documenter@juliadocs.github.io>
2021-12-25 13:36:11 +0000

    build based on dc9c353

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KiteUtils","category":"page"},{"location":"#KiteUtils","page":"Home","title":"KiteUtils","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KiteUtils.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KiteUtils]","category":"page"}]
+}
