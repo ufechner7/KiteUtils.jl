11d942bcdb98794891a8e575b0d3e3cc71621256
Uwe Fechner <fechner@aenarete.eu>
2022-05-08 03:11:44 +0200

    Improve dou, export fromEG2W

--- src/trafo.jl
+++ src/trafo.jl
@@ -91,6 +91,7 @@ end
 
 """
     fromEG2W(vector, down_wind_direction = pi/2.0)
+    
 Transform a vector (x,y,z) from Earth Groundstation to Wind reference frame.
 """
 function fromEG2W(vector, down_wind_direction = pi/2.0)
