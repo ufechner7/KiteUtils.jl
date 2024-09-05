77b454c510ef346381d04f25dbf20cf73fc270b2
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-05 14:07:48 +0200

    fix formatting

--- src/logger.jl
+++ src/logger.jl
@@ -263,7 +263,7 @@ end
     import_log(filename)
 
 Read a .csv file with a flight log and return a SysLog object.
-The columns var_01 to var_05 must exists, the rest are optional.
+The columns `var_01` to `var_05` must exists, the rest are optional.
 
 - filename: name of the file without extension.
 """
