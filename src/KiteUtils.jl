a40438fcc32347ad3c73ee3360d1288bba660e49
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-15 20:04:50 +0200

    fix formatting of doc string

--- src/KiteUtils.jl
+++ src/KiteUtils.jl
@@ -259,9 +259,9 @@ Calculate the initial orientation of the kite based on the last tether segment a
 the apparent wind speed.
 
 Parameters:
-- `vec_c`: (pos_n-2) - (pos_n-1) n: number of particles without the three kite particles
+- `vec_c`: (`pos_n`-2) - (`pos_n`-1) n: number of particles without the three kite particles
                                     that do not belong to the main thether (P1, P2 and P3).
-- v_app: vector of the apparent wind speed
+- `v_app`: vector of the apparent wind speed
 
 Returns:
 x, y, z:  the unit vectors of the kite reference frame in the ENU reference frame
