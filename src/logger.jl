67c73e25fde5cb0090fb1c2a74f613ff9d4bcfc5
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-15 19:13:17 +0200

    fix comment

--- src/logger.jl
+++ src/logger.jl
@@ -265,6 +265,7 @@ end
 Read a .csv file with a flight log and return a SysLog object.
 The columns `var_01` to `var_05` must exists, the rest are optional.
 
+Parameters:
 - filename: name of the file without extension.
 """
 function import_log(filename)
