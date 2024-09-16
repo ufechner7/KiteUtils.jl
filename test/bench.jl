986b8fd92e1ffe8f5b39bc7bcae3d1125c819780
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-09-16 13:26:28 +0200

    minor changes

--- test/bench.jl
+++ test/bench.jl
@@ -1,4 +1,4 @@
-using KiteUtils, BenchmarkTools, StaticArrays
+using KiteUtils, BenchmarkTools, StaticArrays, Test
 
 @benchmark fromKS2EX(vec1, orient) setup=(vec1 = SVector(1.0, 2.0, 3.0); orient = SVector(0, pi/10, pi / 2.0))
 # 64 ns; Python: 10 µs
@@ -7,3 +7,5 @@ res = @benchmark rot(pos_kite, pos_before, v_app) setup=(pos_kite = SVector(1.0,
 @testset "rot" begin
     @test res.allocs == 0
 end
+
+nothing
