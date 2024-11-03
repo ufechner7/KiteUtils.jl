895ff285ba3838a161699232aa1444ec762aaefb
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-03 06:02:44 +0100

    add menu.jl

---
+++ examples/menu.jl
@@ -0,0 +1,22 @@
+using REPL.TerminalMenus
+
+options = ["bridle_info = include(\"bridle_info.jl\")",
+           "import_csv = include(\"import_csv.jl\")",
+           "quit"]
+
+function menu()
+    active = true
+    while active
+        menu = RadioMenu(options, pagesize=8)
+        choice = request("\nChoose function to execute or `q` to quit: ", menu)
+
+        if choice != -1 && choice != length(options)
+            eval(Meta.parse(options[choice]))
+        else
+            println("Left menu. Press <ctrl><d> to quit Julia!")
+            active = false
+        end
+    end
+end
+
+menu()
