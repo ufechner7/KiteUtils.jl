bdbf9e84ec8f575eebf86bd38b9d9920e6c4eaa8
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-03 06:16:54 +0100

    add module doc string

--- src/KiteUtils.jl
+++ src/KiteUtils.jl
@@ -1,3 +1,10 @@
+"""
+    KiteUtils
+
+This module provides data structures for the flight state and the flight log, 
+functions for creating a demo flight state, demo flight log, loading and saving flight logs, 
+functions for reading the settings, and helper functions for working with rotations.
+"""
 module KiteUtils
 
 #= MIT License
