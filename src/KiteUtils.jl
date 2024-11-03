100e05ace7c03d6a5d27e96e9a6d90a6b6a35e34
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-03 06:22:38 +0100

    improve docu

--- src/KiteUtils.jl
+++ src/KiteUtils.jl
@@ -1,9 +1,13 @@
 """
     KiteUtils
 
+Utility functions for the kite simulators.
+
 This module provides data structures for the flight state and the flight log, 
 functions for creating a demo flight state, demo flight log, loading and saving flight logs, 
 functions for reading the settings, and helper functions for working with rotations.
+
+See https://ufechner7.github.io/KiteUtils.jl/stable/ for more information.
 """
 module KiteUtils
 
