c0806aa99880565509cb71f2a7995943289ee1b8
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-03 06:02:31 +0100

    improve example

--- examples/import_csv.jl
+++ examples/import_csv.jl
@@ -5,4 +5,6 @@ set_data_path("data")
 filename="transition"
 
 log = import_log(filename)
+println("Imported arrow log file: ", filename * ".arrow")
 save_log(log)
+println("Saved log file as: ", filename * ".csv")
