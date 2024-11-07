5e8fd46d5a9e66ce551c19dfc0663ab7570bd51a
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-07 17:56:02 +0100

    first include fine now

--- src/build.jl
+++ src/build.jl
@@ -6,14 +6,17 @@ include("yaml_utils.jl")
 HEADER = "Base.@kwdef mutable struct SysState{P}"
 FOOTER = "end"
 inputfile = joinpath("data", "sysstate.yaml")
+outputfile = joinpath("src", "sysstate.jl")
 
 # read the file sysstate.yaml
 sysstate = YAML.load_file(inputfile, dicttype=OrderedDict{String,Any})["sysstate"]
 lines = readfile(inputfile)
-println(HEADER)
-for key in keys(sysstate)
-    comment = get_comment(lines, key)
-    println("    " * comment)
-    println("    " * key * "::" * sysstate[key])
+open(outputfile,"w") do io
+    println(io, HEADER)
+    for key in keys(sysstate)
+        comment = get_comment(lines, key)
+        println(io, "    " * comment)
+        println(io, "    " * key * "::" * sysstate[key])
+    end
+    println(io, FOOTER)
 end
-println(FOOTER)
--- src/sysstate.jl
+++ src/sysstate.jl
@@ -1,7 +1,7 @@
 Base.@kwdef mutable struct SysState{P}
     "time since start of simulation in seconds"
     time::Float64
-    "time needed for one simulation timestep"
+    "time needed for one simulation timestep in seconds"
     t_sim::Float64
     "state of system state control"
     sys_state::Int16
@@ -9,13 +9,13 @@ Base.@kwdef mutable struct SysState{P}
     e_mech::Float64
     "orientation of the kite (quaternion, order w,x,y,z)"
     orient::MVector{4, Float32}
-    "elevation angle in radians"
+    "elevation angle [rad]"
     elevation::MyFloat
-    "azimuth angle in radians"
+    "azimuth angle in wind reference frame [rad]"
     azimuth::MyFloat
     "tether length [m]"
     l_tether::MyFloat
-    "reel out velocity [m/s]"
+    "reelout speed [m/s]"
     v_reelout::MyFloat
     "tether force [N]"
     force::MyFloat
@@ -23,40 +23,56 @@ Base.@kwdef mutable struct SysState{P}
     depower::MyFloat
     "steering settings [-1..1]"
     steering::MyFloat
-    "heading angle in radian"
+    "heading angle [rad]"
     heading::MyFloat
-    "course angle in radian"
+    "course angle [rad]"
     course::MyFloat
     "norm of apparent wind speed [m/s]"
     v_app::MyFloat
-    "wind velocity at ground level [m/s]"
-    v_wind_gnd::MVector{3, MyFloat}
-    "wind velocity at 200m height [m/s]"
-    v_wind_200m::MVector{3, MyFloat}
-    "wind velocity at kite height [m/s]"
-    v_wind_kite::MVector{3, MyFloat}
-    "velocity vector of the kite"
-    vel_kite::MVector{3, MyFloat}
-    "vector of particle positions in x"
-    X::MVector{P, MyFloat}
-    "vector of particle positions in y"
-    Y::MVector{P, MyFloat}
-    "vector of particle positions in z"
-    Z::MVector{P, MyFloat}
+    "wind vector at reference height [m/s]"
+    v_wind_gnd::MVectors{3, MyFloat}
+    "wind vector at 200m height [m/s]"
+    v_wind_200m::MVectors{3, MyFloat}
+    "wind vector at the height of the kite [m/s]"
+    v_wind_kite::MVectors{3, MyFloat}
+    "velocity vector of the kite [m/s]"
+    vel_kite::MVectors{3, MyFloat}
+    "vector of particle positions in x [m]"
+    X::MVectors{P, MyFloat}
+    "vector of particle positions in y [m]"
+    Y::MVectors{P, MyFloat}
+    "vector of particle positions in z [m]"
+    Z::MVectors{P, MyFloat}
+    "generic variable 01"
     var_01::MyFloat
+    "generic variable 02"
     var_02::MyFloat
+    "generic variable 03"
     var_03::MyFloat
+    "generic variable 04"
     var_04::MyFloat
+    "generic variable 05"
     var_05::MyFloat
+    "generic variable 06"
     var_06::MyFloat
+    "generic variable 07"
     var_07::MyFloat
+    "generic variable 08"
     var_08::MyFloat
+    "generic variable 09"
     var_09::MyFloat
+    "generic variable 10"
     var_10::MyFloat
+    "generic variable 11"
     var_11::MyFloat
+    "generic variable 12"
     var_12::MyFloat
+    "generic variable 13"
     var_13::MyFloat
+    "generic variable 14"
     var_14::MyFloat
+    "generic variable 15"
     var_15::MyFloat
+    "generic variable 16"
     var_16::MyFloat
 end
