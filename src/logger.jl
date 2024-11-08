bd691722a7b391698d4bc185ee873fc6f019c658
Uwe Fechner <u.fechner-1@tudelft.nl>
2024-11-08 19:12:50 +0100

    add _syslog.jl

--- src/logger.jl
+++ src/logger.jl
@@ -9,16 +9,7 @@ function length(logger::Logger)
     logger.index - 1
 end
 
-function syslog(logger::Logger)
-    l = logger
-    StructArray{SysState{l.points}}((l.time_vec, l.t_sim_vec, l.sys_state_vec, l.e_mech_vec, l.orient_vec, 
-                l.elevation_vec, l.azimuth_vec, l.l_tether_vec,
-                l.v_reelout_vec, l.force_vec, l.depower_vec, l.steering_vec, l.heading_vec, l.course_vec,
-                l.v_app_vec, l.v_wind_gnd_vec, l.v_wind_200m_vec, l.v_wind_kite_vec, 
-                l.vel_kite_vec, l.X_vec, l.Y_vec, l.Z_vec, l.var_01_vec, l.var_02_vec, l.var_03_vec, 
-                l.var_04_vec, l.var_05_vec, l.var_06_vec, l.var_07_vec, l.var_08_vec, l.var_09_vec, l.var_10_vec,
-                l.var_11_vec, l.var_12_vec, l.var_13_vec, l.var_14_vec, l.var_15_vec, l.var_16_vec))
-end
+include("_syslog.jl")
 
 """
     sys_log(logger::Logger, name="sim_log";
