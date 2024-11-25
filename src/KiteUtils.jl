b5284a9ccc3fa4a1ea6e0d0175caba17a497fe3b
Uwe Fechner <fechner@aenarete.eu>
2024-11-26 00:05:35 +0100

    Update docu

--- src/KiteUtils.jl
+++ src/KiteUtils.jl
@@ -464,16 +464,26 @@ include("_load_log.jl")
     calculate_rotational_inertia(X::Vector, Y::Vector, Z::Vector, M::Vector, around_center_of_mass::Bool=true, 
     rotation_point::Vector=[0, 0, 0])
 
-Calculate the rotational inertia (Ixx, Ixy, Ixz, Iyy, Iyz, Izz) of a colection of point masses around a point. 
+Calculate the rotational inertia (Ixx, Ixy, Ixz, Iyy, Iyz, Izz) of a collection of point masses around a point. 
 By default this point is the center of mass which will be calculated, but any point can be given to rotation_point.
 
-parameters:
+Parameters:
 - X: x-coordinates of the point masses.
 - Y: y-coordinates of the point masses.
 - Z: z-coordinates of the point masses.
 - M: masses of the point masses.
 - around_center_of_mass: Calculate the rotational inertia around the center of mass?
 - rotation_point: Rotation point used if not rotating around the center of mass.
+
+Returns:  
+The tuple  Ixx, Ixy, Ixz, Iyy, Iyz, Izz where:
+- Ixx: rotational inertia around the x-axis.
+- Ixy: rotational inertia around the xy-plane.
+- Ixz: rotational inertia around the xz-plane.
+- Iyy: rotational inertia around the y-axis.
+- Iyz: rotational inertia around the yz-plane.
+- Izz: rotational inertia around the z-axis.
+
 """
 function calculate_rotational_inertia(X::Vector, Y::Vector, Z::Vector, M::Vector, around_center_of_mass::Bool=true, 
     rotation_point::Vector=[0, 0, 0])
